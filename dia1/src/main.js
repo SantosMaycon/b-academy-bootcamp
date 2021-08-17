e23b40a9a2765c0be6be2dbadd40fa683ce06578
SantosMaycon <mayconalves.santos14@gmail.com>
2021-08-17 10:05:24 -0300

    day 1 - add button hiden and show div

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,28 @@
 import "./style.css";
 
-document.querySelector('[data-js="app"]').innerHTML = `
-  <h1>B. Academy</h1>
-  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`;
+const div = document.querySelector('[data-js="app"]');
+const btn = document.querySelector('[data-js="button"]');
+
+const setTextDiv = () => {
+  div.innerHTML = `
+    <h1>B. Academy</h1>
+    <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+  `;
+};
+
+setTextDiv();
+
+const toggle = {
+  "Esconder Div": () => {
+    div.innerHTML = "";
+    btn.innerText = "Div Visível";
+  },
+  "Div Visível": () => {
+    setTextDiv();
+    btn.innerText = "Esconder Div";
+  },
+};
+
+btn.addEventListener("click", () => {
+  toggle[btn.innerText]();
+});
