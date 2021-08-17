0546a5a4c887db7d190be4743ecd943e397f5467
SantosMaycon <mayconalves.santos14@gmail.com>
2021-08-17 11:22:18 -0300

    day 1 - refactor 2 button hiden and show div

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -2,30 +2,25 @@ import "./style.css";
 
 const div = document.querySelector('[data-js="app"]');
 const btn = document.querySelector('[data-js="button"]');
-let hiden = false;
 
-const setTextDiv = () => {
-  div.innerHTML = `
+div.innerHTML = `
     <h1>B. Academy</h1>
     <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
     `;
-};
 
-setTextDiv();
+div.style.display = "block";
 
 const toggle = {
-  false: () => {
-    div.innerHTML = "";
+  block: () => {
+    div.style.display = "none";
     btn.innerText = "Mostrar Div";
-    hiden = true;
   },
-  true: () => {
-    setTextDiv();
+  none: () => {
+    div.style.display = "block";
     btn.innerText = "Esconder Div";
-    hiden = false;
   },
 };
 
 btn.addEventListener("click", () => {
-  toggle[hiden]();
+  toggle[div.style.display]();
 });
