41e9f3001d8b3e41c6be1a0bfa954bf7243b536b
SantosMaycon <mayconalves.santos14@gmail.com>
2021-08-19 14:28:04 -0300

    day 3 - add select multipe color

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -7,6 +7,8 @@ body.innerHTML = `
   </form>
 `;
 
+// Abaixo implementação do input capitalize
+
 const input = body.querySelector('[data-js="input-name"]');
 
 function exceptions(word) {
@@ -29,4 +31,34 @@ function capitalize(word) {
 
 input.addEventListener("input", (event) => {
   event.target.value = capitalize(event.target.value);
+});
+
+// Abaixo implementação do Select Color
+
+const form = body.querySelector("form");
+const createSelect = `
+  </br>
+  </br>
+  <select name="select" multiple data-js="select">
+    <option value="#ff0000">FF0000</option>
+    <option value="#00ff00">00FF00</option>
+    <option value="#0000ff">0000FF</option>
+    <option value="#ffff00">FFFF00</option>
+    <option value="#f000ff">F00FFF</option>
+  </select>
+`;
+form.insertAdjacentHTML("beforeend", createSelect);
+
+const select = document.querySelector('[data-js="select"]');
+body.insertAdjacentHTML("beforeend", '<div data-js="color"></div>');
+
+select.addEventListener("change", (event) => {
+  const colorDiv = document.querySelector('[data-js="color"]');
+  colorDiv.innerHTML = "";
+  console.log([...event.target.selectedOptions].map((color) => color.value));
+  const divs = [...event.target.selectedOptions].map((color) => color.value);
+  divs.forEach((color) => {
+    const div = `<div style="width:100px; height:100px; background:${color}"></div>`;
+    colorDiv.insertAdjacentHTML("beforeend", div);
+  });
 });
