22fcd44985e7fa18a1726d2648d4d902cbe18eee
SantosMaycon <mayconalves.santos14@gmail.com>
2021-08-20 13:52:51 -0300

    day 4 - add fetch GET

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -2,6 +2,36 @@ import "./style.css";
 
 const form = document.querySelector("[data-js='form-cars']");
 const tbody = document.querySelector("[data-js='tbody']");
+const url = "http://localhost:3333/cars";
+const object = {};
+
+const trHead = document.querySelector("[data-js='table'] > thead > tr");
+const th = document.createElement("th");
+th.textContent = "Delete";
+trHead.appendChild(th);
+
+const addObjectValue = {
+  image: (value) => {
+    object.image = value;
+  },
+  model: (value) => {
+    object.brandModel = value;
+  },
+  year: (value) => {
+    const NUMERIC_REGEXP = /[-]{0,1}[\d]*[.]{0,1}[\d]+/g;
+    NUMERIC_REGEXP.test(value) ? (object.year = value) : (object.year = "");
+  },
+  plate: (value) => {
+    const PLATE_REGEXP = /[a-zA-Z]{3}-[0-9]{4}/g;
+    PLATE_REGEXP.test(value)
+      ? (object.plate = value.toUpperCase())
+      : (object.plate = "");
+  },
+  color: (value) => {
+    const COLOR_REGEXP = /[0-9A-Fa-f]{6}/g;
+    COLOR_REGEXP.test(value) ? (object.color = value) : (object.color = "");
+  },
+};
 
 const addElement = {
   image: (value) => {
@@ -21,9 +51,21 @@ const addElement = {
     div.style.margin = "0 auto";
     return createElementTD(div);
   },
-  model: (value) => createTextTD(value),
+  brandModel: (value) => createTextTD(value),
   plate: (value) => createTextTD(value),
   year: (value) => createTextTD(value),
+};
+
+const createTdNone = () => {
+  tbody.innerHTML = "";
+  const tr = document.createElement("tr");
+  const td = document.createElement("td");
+  td.textContent = "Nenhum carro encontrado!";
+  td.setAttribute("colspan", "6");
+  td.style.height = "80px";
+  td.style.color = "#FF0000";
+  tr.appendChild(td);
+  tbody.appendChild(td);
 };
 
 const createTextTD = (text) => {
@@ -39,16 +81,35 @@ const createElementTD = (element) => {
   return td;
 };
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const tr = document.createElement("tr");
-  [...event.target].forEach(({ name, value }, index) => {
-    if (name) tr.appendChild(addElement[name](value));
+const addDeleteButton = (plate) => {
+  const tr = document.querySelector(`#${plate}`);
 
-    name === "color"
-      ? (event.target[index].value = "#000000")
-      : (event.target[index].value = "");
+  const tdButtonDelete = document.createElement("td");
+  const buttonDelete = document.createElement("button");
+  buttonDelete.textContent = "Delete";
+  buttonDelete.addEventListener("click", async () => {
+    await DELETE_CAR(plate);
   });
-  tbody.appendChild(tr);
-  event.target[0].focus();
-});
+
+  tr.appendChild(tdButtonDelete);
+  tdButtonDelete.appendChild(buttonDelete);
+};
+
+const tdObject = (json) => {
+  tbody.innerHTML = "";
+  json.forEach((car) => {
+    const tr = document.createElement("tr");
+    tr.setAttribute("id", car.plate);
+    for (const key in car) tr.appendChild(addElement[key](car[key]));
+    tbody.appendChild(tr);
+    addDeleteButton(car.plate);
+  });
+};
+
+const GET_CARS = async () => {
+  const response = await fetch(url);
+  const json = await response.json();
+  json.length === 0 ? createTdNone() : tdObject(json);
+};
+
+await GET_CARS();
